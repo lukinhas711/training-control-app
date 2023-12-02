f9101984e8ce5c83bdf37d9082eb77295749fb80
Lucas <lucas.ribeiro711@gmail.com>
2023-12-02 14:31:36 -0300

    feat: :zap: install tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,18 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
+  css: ['~/assets/css/main.css'],
+
   modules: [
     // doc: https://nuxt.com/modules/eslint
     '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss'
   ],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
