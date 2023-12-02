ed3c121fce09b55de7b706049e499b2aca4c287a
Lucas <lucas.ribeiro711@gmail.com>
2023-12-02 15:27:32 -0300

    feat: hoot page

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div
+    class="flex flex-col items-center justify-center bg-gradient-to-tr from-gray-900 to-cyan-950 h-screen"
+  >
+    <span class="text-cyan-100"
+      ><Icon name="mingcute:loading-line" class="w-6 h-6 animate-spin" /> Site
+      em construção</span
+    >
+    <NuxtLink
+      external
+      to="https://lucasribeiro.dev.br"
+      class="flex flex-col items-center gap-3 duration-300"
+    >
+      <Icon name="healthicons:gym-outline" class="w-20 h-20 text-cyan-100" />
+    </NuxtLink>
   </div>
 </template>
