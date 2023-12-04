b9fa7d40e9b45b605ed6d562d3cd639895e3457c
Lucas <lucas.ribeiro711@gmail.com>
2023-12-03 22:11:26 -0300

    feat: teste do botão

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,27 @@
   <div
     class="flex flex-col items-center gap-6 justify-center bg-gradient-to-tr from-gray-900 to-cyan-950 h-screen"
   >
-    <span class="text-cyan-100 flex items-center justify-center gap-2">
-      <Icon name="mingcute:loading-line" class="w-6 h-6 animate-spin" /> Site em
-      construção
-    </span>
-    <NuxtLink
-      external
-      to="https://lucasribeiro.dev.br"
-      class="flex flex-col items-center gap-3 duration-300"
+    <BaseButton
+      icon="material-symbols-light:10k"
+      :error="error"
+      :success="success"
+      @click="request"
     >
-      <Icon name="healthicons:gym-outline" class="w-20 h-20 text-cyan-100" />
-    </NuxtLink>
+      Teste o botão
+    </BaseButton>
   </div>
 </template>
+
+<script setup lang="ts">
+const success = ref(false)
+const error = ref(false)
+const response = ref(500)
+
+const request = (): void => {
+  if (response.value === 200) {
+    success.value = true
+  } else {
+    error.value = true
+  }
+}
+</script>
